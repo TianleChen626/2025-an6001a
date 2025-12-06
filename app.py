5e0b80611f91247aafd8ba961ee7ada7f959ad71
TianleChen626 <ctl990626@163.com>
2025-12-06 03:48:23 +0000

    1

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, request, render_template
 
 app = Flask( __name__)
-
-@app.route("/",methods=["GET","POST" ])
+@app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
-@app.route("/main",methods=["GET","POST" ])
+@app.route("/main",methods=["GET","POST"])
 def main():
     q = request.form.get("q")
     print(q)
